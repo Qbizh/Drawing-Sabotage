7be195139cbed88982632c9f5eb333af58805cb5
Qbizh <bobspickleshack@gmail.com>
2025-07-14 17:15:05 -0400

    beach

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Timers;
 using FishNet;
 using FishNet.Connection;
@@ -6,18 +7,44 @@ using FishNet.Object;
 using FishNet.Object.Synchronizing;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+using static GameManager;
 
 public class GameManager : NetworkBehaviour
 {
     public static GameManager instance;
 
-    float roundTime = 15;
+    public static event Action<GameState, bool> gameStateStart;
+
+    GameObject[] gameScenes = new GameObject[3];
+
+    float[] periodTimes = new float[3];
 
     [SerializeField] CardDatabase cardDatabase;
 
     [SerializeField] TMP_Text timerDisplay;
 
-    public readonly SyncTimer gameTimer;
+    [SerializeField] Animator scenesAnimator;
+
+    public readonly SyncTimer periodTimer = new SyncTimer();
+
+    Texture2D[] playerDrawings;
+
+    HashSet<PlayerData> loadedPlayers;
+    
+    public enum GameState 
+    { 
+        Prompt,
+        Game,
+        Voting
+    }
+
+    int currentRound = 0;
+
+    public readonly SyncVar<GameState> gameState = new SyncVar<GameState>();
+    public readonly SyncVar<GameState> lastGameState = new SyncVar<GameState>();
+
+    private GameObject currentScene;
 
     public override void OnStartNetwork()
     {
@@ -32,34 +59,167 @@ public class GameManager : NetworkBehaviour
         }
     }
 
-    public void InitializeGame()
+    public override void OnStartServer()
     {
-        InitializeGameClient();
+        base.OnStartServer();
 
-        gameTimer.StartTimer(roundTime);
+        loadedPlayers = new HashSet<PlayerData>(LobbyManager.instance.players.Count);
+    }
+
+    public void Init()
+    {
+        periodTimer.OnChange += OnTimerChanged;
+
+        InitializeRound();
+    }
+
+    private void InitializeRound()
+    {
+        currentRound++;
+
+        if (currentRound == 1)
+        {
+            gameState.Value = GameState.Prompt;
+            currentScene = gameScenes[(int)gameState.Value];
+            currentScene.SetActive(true);
+
+            TransitionToFirstRound();
+        } else
+        {
+            TransitionState(GameState.Prompt);
+        }
     }
 
     [ObserversRpc]
-    public void InitializeGameClient()
+    private void TransitionToFirstRound()
     {
-        CardsManager.instance.Init(cardDatabase.GetDeck(PlayerDataHolder.instance.playerData.deck));
+        currentScene = gameScenes[(int)gameState.Value];
+        currentScene.SetActive(true);
 
-        foreach (var client in InstanceFinder.ClientManager.Clients)
+        scenesAnimator.SetTrigger("FirstLoad");
+    }
+
+    private void OnTimerChanged(SyncTimerOperation op, float last, float next, bool asServer)
+    {
+        if (asServer && op == SyncTimerOperation.Finished) 
         {
-            if (client.Value != InstanceFinder.ClientManager.Connection)
+            Debug.Log("PERIOD OVER");
+
+            if (gameState.Value == GameState.Voting)
             {
-                PipesManager.instance.AddPlayerPipe(client.Value);
+                // collect results here 
+
+                InitializeRound();
             }
         }
     }
 
     private void Update()
     {
-        gameTimer.Update();
+        if (!periodTimer.Paused)
+        {
+            periodTimer.Update();
 
-        TimeSpan time = TimeSpan.FromSeconds(gameTimer.Remaining);
-        string display = time.ToString("m\\:ss");
+            TimeSpan time = TimeSpan.FromSeconds(periodTimer.Remaining);
+            string display = time.ToString("m\\:ss");
 
-        timerDisplay.text = display;
+            timerDisplay.text = display;
+        }
     }
+
+    [Server]
+    private void TransitionState(GameState newState)
+    {
+        lastGameState.Value = gameState.Value;
+        gameState.Value = newState;
+
+        StartClientLoad();
+    }
+
+
+    [Server]
+    private bool AllClientsLoaded(PlayerData player)
+    {
+        loadedPlayers.Add(player);
+
+        if (loadedPlayers.Count == LobbyManager.instance.players.Count)
+        {
+            loadedPlayers.Clear();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    [ObserversRpc]
+    private void StartClientLoad()
+    {
+        scenesAnimator.SetTrigger("Load");
+    }
+
+
+
+    public void OnLoadIn()
+    {
+        scenesAnimator.speed = 0;
+
+        ClientLoadedIn(PlayerDataHolder.instance.playerData);
+    }
+
+    [ServerRpc]
+    private void ClientLoadedIn(PlayerData player)
+    {
+        if (AllClientsLoaded(player)) 
+        {
+            if (currentScene != null) 
+            {
+                currentScene.SetActive(false);
+            }
+
+            currentScene = gameScenes[(int)gameState.Value];
+
+            currentScene.SetActive(true);
+
+            ContinueClientLoad();
+        }
+    }
+
+
+    [ObserversRpc]
+    private void ContinueClientLoad()
+    {
+        if (currentScene != null)
+        {
+            currentScene.SetActive(false);
+        }
+
+        currentScene = gameScenes[(int)gameState.Value];
+
+        currentScene.SetActive(true);
+
+        scenesAnimator.speed = 1;
+    }
+
+    public void OnLoadOut()
+    {
+        ClientFinishedLoad(PlayerDataHolder.instance.playerData);
+    }
+
+    [ServerRpc]
+    private void ClientFinishedLoad(PlayerData player)
+    {
+        if (AllClientsLoaded(player))
+        {
+            gameStateStart?.Invoke(gameState.Value, true); // currentState, isServer
+            StartStateClient();
+        }
+    }
+
+    [ObserversRpc]
+    private void StartStateClient()
+    {
+        gameStateStart?.Invoke(gameState.Value, false);
+    }
+   
 }
