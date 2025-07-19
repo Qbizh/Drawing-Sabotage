028b59238ebef494efb411db0f1235bafe990c70
Qbizh <132625550+Qbizh@users.noreply.github.com>
2025-07-18 20:04:30 -0400

    prompt work

--- Assets/Scripts/Game/PromptPhaseHandler.cs
+++ Assets/Scripts/Game/PromptPhaseHandler.cs
@@ -1,6 +1,36 @@
 using UnityEngine;
+using System.Collections.Generic;
+using FishNet.Object;
 
 public class PromptPhaseHandler : PhaseHandler
 {
-    
+    public HashSet<string> tags = new HashSet<string>();
+
+    [Server]
+    public override void StartPhase()
+    {
+        tags = GamePhaseManager.instance.gameDataHolder.GetUnfulfilledFormatTags();
+
+        base.StartPhase();  
+    }
+
+    [ObserversRpc]
+    public void UpdateTagsClient(List<string> newTags)
+    {
+        //tags = newTags.to;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void AddPromptInput(string tag, string input)
+    {
+        if (tags.Contains(tag))
+        {
+            bool aFormatIsFulfilled = GamePhaseManager.instance.gameDataHolder.AddPromptInput(tag, input);
+
+            if (aFormatIsFulfilled && phaseTimer.Paused)        // returns true if the added input makes a format fulfilled meaning we can start the countdown to next phase
+            {
+                StartPhaseTimer();
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/UI/PromptInputer.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PromptInputer : MonoBehaviour
+{
+    [SerializeField] PromptPhaseHandler promptPhaseHandler;
+
+
+
+    public void OnPromptEnter()
+    {
+        
+    }
+}
